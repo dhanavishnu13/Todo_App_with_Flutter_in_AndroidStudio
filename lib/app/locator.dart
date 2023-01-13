242ec6a46d40e9e77be357cf764b5dedf5e511de
dhanavishnu13 <vishnukvka@gmail.com>
2023-01-13 10:15:03 +0530

    Revert "Initial commit"
    This reverts commit 98c61c5e7656759f8bd8dbf9bf383e8200bad6d2.

--- lib/app/locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-
-import '../services/todos.service.dart';
-
-final locator = GetIt.instance;
-
-setupLocator() {
-  locator.registerLazySingleton(() => TodosService());
-}
--- lib/models/todo.dart
+++
@@ -1,7 +0,0 @@
-class Todo {
-  final String id;
-  bool completed;
-  String content;
-
-  Todo({required this.id, this.completed = false, this.content = ''});
-}
